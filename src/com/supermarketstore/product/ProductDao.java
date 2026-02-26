bed778b327557f69746def00591002ad4a65236e
nikitsya <nikitasmiichyk@gmail.com>
2026-02-26 20:54:27 +0000

    define ProductDao interface methods for CRUD and filtering

--- src/com/supermarketstore/product/ProductDao.java
+++ src/com/supermarketstore/product/ProductDao.java
@@ -1,5 +1,17 @@
 package com.supermarketstore.product;
 
-public interface ProductDao {
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
+public interface ProductDao {
+    List<Product> getAllProducts();
+    Optional<Product> getProductById(int id);
+    boolean deleteProductById(int id);
+    // TODO insertProduct(Product product);
+    // TODO updateProduct(int id, Product product);
+    List<Product> findProductsByFilter(Predicate<Product> filter);
+    String productToJson(Product entity);
+    Product productFromJson(String json);
+    String productListToJson(List<Product> list);
 }
