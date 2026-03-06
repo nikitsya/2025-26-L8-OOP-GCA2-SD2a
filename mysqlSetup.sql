c2dfc868704fa06d67e26679d30c9e770a137780
nikitsya <nikitasmiichyk@gmail.com>
2026-03-06 20:52:08 +0000

    change database names

--- mysqlSetup.sql
+++ mysqlSetup.sql
@@ -2,12 +2,12 @@ DROP DATABASE IF EXISTS supermarket_store_system;
 CREATE DATABASE supermarket_store_system;
 USE supermarket_store_system;
 
-CREATE TABLE department(
+CREATE TABLE departments (
     department_id INT AUTO_INCREMENT PRIMARY KEY
     # TODO: (Hanna)
 );
 
-CREATE TABLE product (
+CREATE TABLE products (
      product_id INT AUTO_INCREMENT PRIMARY KEY,
      name VARCHAR(100) NOT NULL,
      price DOUBLE NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE product (
      discount_price DOUBLE,
      stock INT NOT NULL,
      department_id INT,
-     FOREIGN KEY (department_id) REFERENCES department(department_id)
+     FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
 # TODO: (Hanna) department Insert > 10 seed rows
