835325a8a144229598c25879b0bcd441a386bb72
HannaBProjects <hannabokariuk@gmail.com>
2026-03-07 12:50:59 +0000

    Add Department name trimming test

--- src/test/java/com/supermarketstore/department/DepartmentTest.java
+++ src/test/java/com/supermarketstore/department/DepartmentTest.java
@@ -1,4 +1,15 @@
 package com.supermarketstore.department;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DepartmentTest {
+
+    @Test
+    void shouldTrimDepartmentName() {
+        Department department = new Department(1, "  Bakery  ", 0, 2, 12000.0, 5, false);
+
+        assertEquals("Bakery", department.getName());
+    }
 }
