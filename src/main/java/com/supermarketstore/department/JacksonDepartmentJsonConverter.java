46cad888f0041190e07d3b3b6705b0930d633117
HannaBProjects <hannabokariuk@gmail.com>
2026-03-07 02:59:53 +0000

    Add ObjectMapper to JacksonDepartmentJsonConverter

--- src/main/java/com/supermarketstore/department/JacksonDepartmentJsonConverter.java
+++ src/main/java/com/supermarketstore/department/JacksonDepartmentJsonConverter.java
@@ -1,8 +1,13 @@
 package com.supermarketstore.department;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.List;
 
 public class JacksonDepartmentJsonConverter implements DepartmentJsonConverter {
+
+    private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
 
     @Override
     public String departmentToJson(Department entity) {
