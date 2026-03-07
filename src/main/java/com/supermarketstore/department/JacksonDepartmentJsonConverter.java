f6734b00d4f068e18ecaa5ab7d44ef55540b95e0
HannaBProjects <hannabokariuk@gmail.com>
2026-03-07 02:57:24 +0000

    Add JacksonDepartmentJsonConverter class

---
+++ src/main/java/com/supermarketstore/department/JacksonDepartmentJsonConverter.java
@@ -0,0 +1,21 @@
+package com.supermarketstore.department;
+
+import java.util.List;
+
+public class JacksonDepartmentJsonConverter implements DepartmentJsonConverter {
+
+    @Override
+    public String departmentToJson(Department entity) {
+        return null;
+    }
+
+    @Override
+    public Department departmentFromJson(String json) {
+        return null;
+    }
+
+    @Override
+    public String departmentListToJson(List<Department> list) {
+        return null;
+    }
+}
