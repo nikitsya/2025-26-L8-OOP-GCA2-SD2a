a4d24c7d110ae06261ea0e1682cab31747883c54
HannaBProjects <hannabokariuk@gmail.com>
2026-03-07 02:00:59 +0000

    create Department class with field

---
+++ src/main/java/com/supermarketstore/department/Department.java
@@ -0,0 +1,12 @@
+package com.supermarketstore.department;
+
+public class Department {
+    private int departmentId;
+    private String name;
+    private int floor;
+    private int zone;
+    private double budget;
+    private int employeeCount;
+    private boolean isRefrigerated;
+
+}
