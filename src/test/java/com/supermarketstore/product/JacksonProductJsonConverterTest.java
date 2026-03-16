8968fd450e2ed7222bdf0d896b8dafc380ff5290
nikitsya <nikitasmiichyk@gmail.com>
2026-03-16 18:38:46 +0000

    add JacksonProductJsonConverterTest skeleton

---
+++ src/test/java/com/supermarketstore/product/JacksonProductJsonConverterTest.java
@@ -0,0 +1,30 @@
+package com.supermarketstore.product;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JacksonProductJsonConverterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void productToJson() {
+    }
+
+    @Test
+    void productFromJson() {
+    }
+
+    @Test
+    void productListToJson() {
+    }
+}
