df23414700944ee85fc0f761168ff06e6bad8bc9
nikitsya <nikitasmiichyk@gmail.com>
2026-03-16 19:33:51 +0000

    add productListToJson serialization test

--- src/test/java/com/supermarketstore/product/JacksonProductJsonConverterTest.java
+++ src/test/java/com/supermarketstore/product/JacksonProductJsonConverterTest.java
@@ -2,6 +2,9 @@ package com.supermarketstore.product;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JacksonProductJsonConverterTest {
@@ -9,7 +12,7 @@ class JacksonProductJsonConverterTest {
     JacksonProductJsonConverter converter = new JacksonProductJsonConverter();
     Product  product = new Product(1, "cucumber", 0.65, false, null, 98);
     String jsonString = "{\"product_id\":1,\"name\":\"cucumber\",\"price\":0.65,\"is_on_sale\":false,\"discount_price\":null,\"stock\":98}";
-
+    List<Product> productList = new ArrayList<>();
 
     @Test
     void productToJson() {
@@ -23,5 +26,9 @@ class JacksonProductJsonConverterTest {
 
     @Test
     void productListToJson() {
+        productList.add(product);
+        productList.add(product);
+        String expected = "[" + jsonString + "," + jsonString + "]";
+        assertEquals(converter.productListToJson(productList), expected);
     }
 }
