a9749105343cabf8ea5cd25fb079e7458eac9b5d
nikitsya <nikitasmiichyk@gmail.com>
2026-03-16 18:53:03 +0000

    implement productToJson test in JacksonProductJsonConverterTest

--- src/test/java/com/supermarketstore/product/JacksonProductJsonConverterTest.java
+++ src/test/java/com/supermarketstore/product/JacksonProductJsonConverterTest.java
@@ -1,6 +1,5 @@
 package com.supermarketstore.product;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,20 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JacksonProductJsonConverterTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    JacksonProductJsonConverter converter = new JacksonProductJsonConverter();
+    Product  product = new Product(1, "cucumber", 0.65, false, null, 98);
+    String jsonString = "{\"product_id\":1,\"name\":\"cucumber\",\"price\":0.65,\"is_on_sale\":false,\"discount_price\":null,\"stock\":98}";
 
-    @AfterEach
-    void tearDown() {
-    }
 
     @Test
     void productToJson() {
+        assertEquals(converter.productToJson(product), jsonString);
     }
 
     @Test
     void productFromJson() {
+
     }
 
     @Test
