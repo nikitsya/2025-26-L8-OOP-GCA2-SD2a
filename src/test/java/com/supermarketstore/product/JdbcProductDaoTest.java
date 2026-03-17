7a06be07fb19186fe2d659c768a3a1f95a84dabc
nikitsya <nikitasmiichyk@gmail.com>
2026-03-17 13:41:45 +0000

    add JdbcProductDaoTest skeleton with empty test stubs

---
+++ src/test/java/com/supermarketstore/product/JdbcProductDaoTest.java
@@ -0,0 +1,44 @@
+package com.supermarketstore.product;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JdbcProductDaoTest {
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void deleteProductById() {
+    }
+
+    @Test
+    void insertProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void findProductsByFilter() {
+    }
+
+    @Test
+    void _url() {
+    }
+
+    @Test
+    void _user() {
+    }
+
+    @Test
+    void _pass() {
+    }
+}
