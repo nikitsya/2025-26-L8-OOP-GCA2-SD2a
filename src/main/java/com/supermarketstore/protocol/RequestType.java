3096b0444418c6d9be6dedb253235b94c674109e
nikitsya <nikitasmiichyk@gmail.com>
2026-03-25 16:43:30 +0000

    add RequestType enum for protocol request names

---
+++ src/main/java/com/supermarketstore/protocol/RequestType.java
@@ -0,0 +1,13 @@
+package com.supermarketstore.protocol;
+
+/**
+ * Enumerates the supported JSON protocol request types.
+ */
+public enum RequestType {
+    GET_ALL_DEPARTMENTS,
+    GET_DEPARTMENT_BY_ID,
+    ADD_DEPARTMENT,
+    GET_ALL_PRODUCTS,
+    GET_PRODUCT_BY_ID,
+    ADD_PRODUCT
+}
